a09bda5366560b6ab1bf0274ae6367e422020ed5
Anzo52 <andy@zollnersolutions.com>
2023-12-31 13:35:36 -0700

    add detect_batch.py

---
+++ detect_batch.py
@@ -0,0 +1,39 @@
+from detect_people import detect_people
+import os
+
+
+def detect_in_dir(dir_path):
+    file_paths = []
+    with_people = []
+    no_people = []
+    for file_name in os.listdir(dir_path):
+        print(file_name)
+        file_paths.append(os.path.join(dir_path, file_name))
+    for file in file_paths:
+        if detect_people(file):
+            print(f"People detected in {file}")
+            with_people.append(file)
+        else:
+            print(f"No people detected in {file}")
+            no_people.append(file)
+    return with_people, no_people
+
+
+def file_dialog():
+    from tkinter import Tk
+    from tkinter.filedialog import askdirectory
+
+    Tk().withdraw()
+    return askdirectory()
+
+
+def main():
+    dir_path = file_dialog()
+    with_people, no_people = detect_in_dir(dir_path)
+    print(f"{len(with_people)} Files with people: {with_people}")
+    print(f"{len(no_people)} Files without people: {no_people}")
+    print(f"Total files: {len(with_people) + len(no_people)}")
+
+
+if __name__ == "__main__":
+    main()
