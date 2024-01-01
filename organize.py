0aee1493c912bff781cc342d71c0c0e85dab06bc
Anzo52 <andy@zollnersolutions.com>
2024-01-01 13:06:08 -0700

    remove the redundant functions

--- organize.py
+++ organize.py
@@ -20,6 +20,12 @@ def make_with_people_dir(dir_path):
     return with_people_dir
 
 
+def make_directory(dir_path, dir_name):
+    directory = os.path.join(dir_path, dir_name)
+    os.makedirs(directory, exist_ok=True)
+    return directory
+
+
 def move_files(file_list, dir_path):
     for file in file_list:
         print(f"Moving {file} to {dir_path}")
@@ -29,12 +35,15 @@ def move_files(file_list, dir_path):
 def main():
     dir_path = file_dialog()
     with_people, no_people = detect_in_dir(dir_path)
-    no_people_dir = make_no_people_dir(dir_path)
-    with_people_dir = make_with_people_dir(dir_path)
-    move_files(no_people, no_people_dir)
-    move_files(with_people, with_people_dir)
-    print(f"{len(with_people)} Files with people: {with_people}")
-    print(f"{len(no_people)} Files without people: {no_people}")
+    directories = {
+        "with_people": with_people,
+        "no_people": no_people,
+    }
+    for dir_name, files in directories.items():
+        target_dir = make_directory(dir_path, dir_name)
+        move_files(files, target_dir)
+    print(f"(len(with_people)) Files with people: {with_people}")
+    print(f"(len(no_people)) Files without people: {no_people}")
     print(f"Total files: {len(with_people) + len(no_people)}")
 
 
