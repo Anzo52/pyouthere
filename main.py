72da237a4913be8ac57c3bef76171b0b3c87e62f
Anzo52 <andy@zollnersolutions.com>
2024-01-01 13:25:40 -0700

    add print_file_count function

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from dialogs import select_dialog
 from organize import make_directory, move_files
 
 
+def print_file_count(with_people, no_people):
+    print(f"{len(with_people)} Files with people: {with_people}")
+    print(f"{len(no_people)} Files without people: {no_people}")
+    print(f"Total files: {len(with_people) + len(no_people)}")
+
 def detect_in_directory():
     dir_path = select_dialog("directory")
     with_people, no_people = detect_in_dir(dir_path)
